7a48d47cd08f403b77df603d5920109cd15b6402
Ben Egloff <begloff@nd.edu>
2023-04-01 01:07:20 +0000

    Scraper Work

--- scrape.py
+++ scrape.py
@@ -1,14 +1,36 @@
 import requests
 import html5print
+import re
 
 
 x = requests.get('https://exrx.net/Lists/Directory')
 html = x.text
 
 #Split everything based on list tags
+print(html)
+# print(html5print.HTMLBeautifier.beautify(html, 4))
+generalmuscles = re.findall(r"<li.*><a.*>(.*?)</a>\n<ul>", html, re.MULTILINE)
 
-print(html5print.HTMLBeautifier.beautify(html, 4))
+print()
+print(generalmuscles)
+temp = {}
 
-y = html.split('<li ')
-for entry in y:
-    print(entry)
+for i in range(len(generalmuscles)):
+    if i != len(generalmuscles) - 1:
+        regex = re.escape(generalmuscles[i]) + r"(.*?)" + re.escape(generalmuscles[i+1]) 
+        temp[generalmuscles[i]] = (re.findall(regex, html, re.MULTILINE | re.DOTALL ))
+
+#Temp contains all of the list elements for a given muscle group
+#Need to extract non <a> links and append them to <a> below
+
+
+for i in temp:
+    regex = re.findall(r"<li>(.*?)<", temp[i])
+
+#Strategy: Extract General muscle group --> extract individual list elements from each
+
+# y = html.split('<li ')
+# for entry in y:
+#     print(entry)
+
+print()
