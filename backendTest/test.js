f1f5a62ad0930531f5c3512688a0ddca186c3d4a
Ben Egloff <yobegloff@nd.edu>
2023-05-03 03:34:19 +0000

    minor

--- backendTest/test.js
+++ backendTest/test.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-console.log('Hello')
-const x = await axios.get("http://34.206.226.160/hello.py?sql=select * from exercises")
+sql = 'delete from log where session_id = 204; delete from sessions where session_id = 204'
+const x = await axios.post(`http://34.206.226.160/hello.py?sql=${sql}`)
 console.log(x.data)
